40c88fcf815c3dfd30ba9164f7f33f048a8b9aeb
SamuelUkachukwu <shybold@yahoo.com>
2022-05-20 17:26:18 +0000

    Add CDN links and templates

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,7 +1,18 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("task.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET","POST"])
+def add_category():
+    return render_template("add_category.html")
