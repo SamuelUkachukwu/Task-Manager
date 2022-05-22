848baa9d74c69749f37df878703b861ac3b48da4
SamuelUkachukwu <shybold@yahoo.com>
2022-05-22 22:25:46 +0000

    Add add_task page and functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: 'dd mmmm, yyyy',
+    i18n: {done:'Select'}
   });
+
+  // 
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
+
